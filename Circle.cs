c78a7b64f3da90816a391fcf45bc02a659f37128
simpson-gary <simpson_gary@aol.com>
2015-03-31 10:27:32 -0400

    Create Circle.cs

---
+++ Circle.cs
@@ -0,0 +1,57 @@
+//Developer 	Gary Simpson
+//Due Date		3-29-2015
+//Name        Circle.cs
+/*Purpose   	Text book exercise 12.10
+
+			
+                12.10 (Shape Hierarchy) Implement the Shape hierarchy of Fig. 11.3.  ---page 408--	
+                   Omit the Triangle and Tetrahedron classes.		
+               Each TwoDimensionalShape should contain read-only abstract property
+                    Area to calculate the area of the two-dimensional shape.
+               Each ThreeDimensionalShape should have read- only abstract properties
+                   Area and Volume to calculate the surface area and volume, respectively,
+                   of the three-dimensional shape. Create an app that uses an array of Shape
+                   references to objects of each concrete class in the hierarchy. 
+                Display a text description of the object to which each array element refers.
+                Also, in the loop that processes all the shapes in the array, determine whether
+                   each shape is a TwoDimensionalShape or a ThreeDimensionalShape.
+                If a shape is a TwoDimensionalShape, display its area.
+                If a shape is a ThreeDimensionalShape, display its area and volume.
+		
+                                NOTE: add Console.ReadKey() to stop program execution in main Method.
+
+*/
+using System;
+
+namespace ShapeHierarchy
+{
+    class Circle : TwoDimensionalShape
+    {
+        private double radius;
+        const string measurementName = "radius";
+         // constructor
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }// end constructor
+
+        public override double Area
+        {
+            get
+            {
+                return CalculateArea();
+            }// end get
+        }//End // override property Area
+
+        public override string MeasurmentName
+        {
+            get { return measurementName; }
+        } // end property MeasurementName
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);      
+        }// end override method CalculateArea
+
+    } // end class Circle
+}
