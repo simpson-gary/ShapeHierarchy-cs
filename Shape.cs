e6975cd73dc627191cb3aed61b221b13dd0c5505
simpson-gary <simpson_gary@aol.com>
2015-03-31 10:20:53 -0400

    Create Shape.cs
    An Abstract that contains abstract properties.

---
+++ Shape.cs
@@ -0,0 +1,52 @@
+//Developer 	Gary Simpson
+//Due Date		3-29-2015
+//Name          Shape.cs
+/*Purpose   	Text book exercise 12.10
+
+			
+                12.10 (Shape Hierarchy) Implement the Shape hierarchy of Fig. 11.3.  ---page 408--	
+                   Omit the Triangle and Tetrahedron classes.		
+               Each TwoDimensionalShape should contain read-only abstract property
+                    Area to calculate the area of the two-dimensional shape.
+               Each ThreeDimensionalShape should have read- only abstract properties
+                   Area and Volume to calculate the surface area and volume, respectively,
+                   of the three-dimensional shape. Create an app that uses an array of Shape
+                   references to objects of each concrete class in the hierarchy. 
+                Display a text description of the object to which each array element refers.
+                Also, in the loop that processes all the shapes in the array, determine whether
+                   each shape is a TwoDimensionalShape or a ThreeDimensionalShape.
+                If a shape is a TwoDimensionalShape, display its area.
+                If a shape is a ThreeDimensionalShape, display its area and volume.
+		
+                                NOTE: add Console.ReadKey() to stop program execution in main Method.
+
+*/
+using System;
+
+namespace ShapeHierarchy
+{
+
+    abstract class Shape : object
+    {
+        // constructor
+        public Shape()
+        {
+            
+        }// end constructor
+
+        // read-only property for Area
+        public abstract double Area
+        {
+            get;
+        }// end property Area
+
+        public abstract string MeasurmentName
+        {
+            get;
+        }// end property MeasurementName
+
+        //abstract method to be overrriden in derived class
+        public abstract double CalculateArea();
+
+    }// end class Shape
+}// end namespace ShapeHierarchy
